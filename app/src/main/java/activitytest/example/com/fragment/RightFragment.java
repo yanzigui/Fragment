b37f7c991ae2a116b663b7f1032301a26ced8650
yzg <yzg@1.com>
2017-07-10 18:17:54 +0800

    Initial commit

---
+++ app/src/main/java/activitytest/example/com/fragment/RightFragment.java
@@ -0,0 +1,20 @@
+package activitytest.example.com.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by yzg on 2017/7/10.
+ */
+
+public class RightFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.right_fragment,container,false);
+        return view;
+    }
+}
